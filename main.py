e911e445699ed73e76daee8dc92726fa05f9c92c
igorFernandesBD <igorfernandestec@gmail.com>
2025-01-27 16:08:48 -0300

    Remoção dos comentários

--- main.py
+++ main.py
@@ -6,27 +6,10 @@ banco = sqlite3.connect('banco.db')
 
 cursor = banco.cursor()
 
-processo = int(input("Qual e o processo a ser feito ?\n [1] - Somar compras\n "))
-
-# processo = int(input('O que deseja fazer ?\n [1] '))
-
-# cursor.execute('CREATE TABLE produtos(ID int NOT NULL PRIMARY KEY, nomeProduto varchar(50) NOT NULL, categoria varchar(50) NOT NULL, refrigerado bit NOT NULL, alcoolico bit NOT NULL )')
-
-# cursor.execute('INSERT INTO produtos(ID, nomeProduto, categoria, refrigerado, alcoolico) VALUES(1, "LeiteIntegral", "Bebidas", 1, 0), (2, "LeiteDesnatado", "Bebidas", 1, 0), (3, "Biscoito", "Alimentos", 0, 0), (4, "Cerveja", "Bebidas", 1, 1), (5, "Picanha", "Carnes", 1, 0)')
-
-# banco.commit()
-
-# cursor.execute('ALTER TABLE produtos ADD valorFixo int')
-
-# cursor.execute('UPDATE produtos SET valorFixo = 5 WHERE nomeProduto = "leiteIntegral"')
-# cursor.execute('UPDATE produtos SET valorFixo = 5 WHERE nomeProduto = "leiteDesnatado"')
-# banco.commit()
-# cursor.execute('UPDATE produtos SET valorFixo = 3 WHERE nomeProduto = "Biscoito"')
-# cursor.execute('UPDATE produtos SET valorFixo = 10 WHERE nomeProduto = "Cerveja"')
-#cursor.execute('UPDATE produtos SET valorFixo = 50 WHERE nomePRoduto = "Picanha"') 
+processo = int(input("Qual e o processo a ser feito ?\n[1] - Somar compras\n "))
 
 if processo == 1:
-    print("teste")
+    print 
 
 cursor.execute('SELECT valorFixo FROM produtos WHERE nomeProduto = "Picanha"') 
 resultado = cursor.fetchone()
@@ -39,8 +22,4 @@ valor2 = int(resultado[0])
 print ("o valor total e: ")
 print (valor1 + valor2)
 
-# print (preco1, preco2)
 
-# cursor.execute('SELECT * FROM produtos')
-
-# print(cursor.fetchall())
