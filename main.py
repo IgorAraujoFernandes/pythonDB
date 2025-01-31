f405ada39e97726abf5041c6d0bbc0b535df7d77
igorFernandesBD <igorfernandestec@gmail.com>
2025-01-31 14:50:02 -0300

    Adição da função de consultar preço do produto

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ banco = sqlite3.connect('banco.db')
 
 cursor = banco.cursor()
 
-global somaTotal, maximo, limite
+global somaTotal, maximo, limite, preço
 
-somaTotal = 0 
+somaTotal = 0
+preço = 0 
 cursor.execute('SELECT id FROM produtos ORDER BY id DESC LIMIT 1')
 maximo = cursor.fetchone()
 limite = int(maximo[0])
 
+
 def somarCompras(produto,):
-    global valorSoma
+   
     cursor.execute('SELECT valorFixo FROM produtos WHERE ID = ?', (produto, ))
     valorProduto = cursor.fetchone()
     valorSoma = int(valorProduto[0])
+    
+    return valorSoma
+    
     
 def inserirProduto():
     nome = input("Digite o nome do produto novo:\n")
@@ -39,13 +44,17 @@ def inserirProduto():
     cursor.execute('INSERT INTO produtos ("ID", "nomeProduto", "categoria", "refrigerado", "alcoolico", "quantidade", "valorFixo") VALUES(?, ?, ?, ?, ?, ?, ?)',(nvID, nome, categoria, refrigerado, alcoolico, quantidade, valorFixo, ))
     banco.commit()
     
-    linhas_afetadas = banco.total_changes
-    print(f'O número de linhas afetadas pela operação foi: {linhas_afetadas}')
-    cursor.execute('SELECT * FROM produtos WHERE ID = ?', (nvID, ))
-    print(cursor.fetchall())
-     
-processo = int(input("Qual e o processo a ser feito ?\n[1] - Somar compras\n[2] - Inserir produto novo\n")) 
-     
+def consultarPreco():
+
+  global preço
+  IDproduto = int(input("Digite o ID do produto a ser consultado:\n"))
+  cursor.execute("SELECT valorFixo FROM produtos WHERE ID = ?", (IDproduto, ))
+  valorFixo = cursor.fetchone()
+  preço = int(valorFixo[0]) 
+  
+  
+processo = int(input("Qual e o processo a ser feito ?\n[1] - Somar compras\n[2] - Inserir produto novo\n[3] - Consultar preço do produto\n")) 
+
 if processo == 1:
   
         valorProduto = [limite]
@@ -65,5 +74,7 @@ if processo == 2:
   inserirProduto()
   print("Produto inserido com sucesso!\n")
   
-
+if processo == 3:
+  consultarPreco() 
+  print(f"O valor desse produto é: R${preço}")
 
