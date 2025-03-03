cfe0483b538ef41064c387450d9a4abdb80e5aa3
IgorFernandes <igorfernandestec@gmail.com>
2025-03-02 23:19:00 -0300

    remocao de um execute problematico

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 
 banco = sqlite3.connect('banco.db')
 cursor = banco.cursor()
@@ -13,17 +14,11 @@ limite = int(maximo[0])
 
 def somarCompras(produto, ):
    
-
     global valorSoma
     cursor.execute('SELECT valorFixo FROM produtos WHERE ID = ?', (produto, ))
     valorProduto = cursor.fetchone()
     valorSoma = int(valorProduto[0])
-<<<<<<< HEAD
-  
-  
-=======
     
->>>>>>> fb2bf7f14882c5e340a7598a23685f9fe15130a8
 def inserirProduto():
     nome = input("Digite o nome do produto novo:\n")
     
@@ -45,32 +40,38 @@ def inserirProduto():
 
     cursor.execute('INSERT INTO produtos ("ID", "nomeProduto", "categoria", "refrigerado", "alcoolico", "quantidade", "valorFixo") VALUES(?, ?, ?, ?, ?, ?, ?)',(nvID, nome, categoria, refrigerado, alcoolico, quantidade, valorFixo, ))
     banco.commit()
-    
+  
 def consultarPreco():
 
-  global preço
+
+  global valorFixo
   IDproduto = int(input("Digite o ID do produto a ser consultado:\n"))
-  cursor.execute("SELECT valorFixo FROM produtos WHERE ID = ?", (IDproduto, ))
-  valorFixo = cursor.fetchone()
-  preço = int(valorFixo[0]) 
+  cursor.execute("SELECT valorFixo FROM produtos WHERE ID = ?", (IDproduto,))
+  novoValorFixo = cursor.fetchone() 
+  valorFixo = int(novoValorFixo[0])
   
+ 
 def atualizar():
   idProduto = int(input("Digite o ID do produto a ser atualizado:\n"))
   print(f"o valor é {idProduto}")
   
   campoAtualizar = (input("\nQual campo você quer atualizar ?\n[1] - Nome [2] - Categoria [3] - Refrigerado [4] - Alcoolico [5] - Quantidade [6] - Valor\n"))
-  #print(f"Campo:", (campoAtualizar, ))
   campoAtualizarINT = int(campoAtualizar) 
   
   if campoAtualizarINT == 3 or campoAtualizarINT == 4:
-    binario = int(input("Escolha o status:\n[1] - Refrigerado    [2] - Não refrigerado\n"))
+    try:
+      binario = int(input("Escolha o status:\n[1] - Refrigerado    [2] - Não refrigerado\n"))
+    except ValueError:
+      print("Numero inválido\n")
     if binario == 2:
       binario = 0
   else:
      atualizacao = input("Qual atualização você deseja fazer?\n")
      if campoAtualizarINT == 5 or campoAtualizarINT == 6:
-      atualizacaoint= int(atualizacao)
-  
+      try:
+        atualizacaoint= int(atualizacao)
+      except ValueError:
+        print("Valor inválido\n")
   if campoAtualizarINT == 1:
     
      cursor.execute("UPDATE produtos SET nomeProduto = ? WHERE ID = ?", (atualizacao, idProduto, ))
@@ -89,7 +90,7 @@ def atualizar():
   if campoAtualizarINT == 3 or campoAtualizarINT == 4:
     
     if campoAtualizarINT == 3:
-     cursor.execute("UPDATE produtos SET refrigerado = ? WHERE ID = ?", (binario, idProduto, ))
+      cursor.execute("UPDATE produtos SET refrigerado = ? WHERE ID = ?", (binario, idProduto, )) 
     else:
       cursor.execute("UPDATE produtos SET alcoolico = ? WHERE ID = ?", (binario, idProduto, ))
   banco.commit()
@@ -116,45 +117,61 @@ def excluir():
   cursor.execute("DELETE FROM produtos WHERE ID = ?", (IDproduto, ))
   banco.commit()
 
+def printID():
+   cursor.execute('SELECT ID, nomeProduto FROM produtos')
+   listaID = (cursor.fetchall())
+   print(f"Os IDs sao:\n{listaID}\n")
 
-processo = int(input("Qual e o processo a ser feito ?\n[1] - Somar compras\n[2] - Inserir produto novo\n[3] - Consultar preço do produto\n[4] - Atualizar campo\n[5] - Excluir campo\n")) 
-
-if processo == 1:
-<<<<<<< HEAD
-        
-=======
-        valorProduto = [limite]
->>>>>>> fb2bf7f14882c5e340a7598a23685f9fe15130a8
-        i = 0
-        
-        print('Digite "FIM" a qualquer momento do programa para finalizar')
-        
-        while i < limite:          
+def main():
+  processo = int(input("Qual e o processo a ser feito ?\n[1] - Somar compras\n[2] - Inserir produto novo\n[3] - Consultar preço do produto\n[4] - Atualizar campo\n[5] - Excluir campo\n")) 
+  if processo != 2:
+    printID()
+    
+  if processo == 1:
+         global valorProduto
+         valorProduto = [limite]
+         i = 0
+         somaTotal = 0
+         print('Digite "FIM" a qualquer momento do programa para finalizar')
           
-            produto = (input("Digite o ID do produto\n"))
-            if produto.upper() == "FIM":
-              break
+         while i < limite:          
             
-            try:
-              produto = int(produto)
-            except ValueError:
-              print("Por favor, insira um número válido para o ID do produto.")  
-              continue
-            somarCompras(produto)
-            somaTotal += valorSoma
-            i += 1
-        print(f"O valor total da compra é:R${somaTotal}\n") 
-                               
-if processo == 2:
-  inserirProduto()
-  print("Produto inserido com sucesso!\n")
+              produto = (input("Digite o ID do produto\n"))
+              if produto.upper() == "FIM":
+                break
+              
+              try:
+                produto = int(produto)
+              except ValueError:
+                print("Por favor, insira um número válido para o ID do produto.")  
+                continue
+              somarCompras(produto)
+              somaTotal += valorSoma
+              i += 1
+              print(f"O valor total da compra é:R${somaTotal}\n")
+              
+                                
+  if processo == 2:
+    inserirProduto()
+    print("Produto inserido com sucesso!\n")
+    
+  if processo == 3:
+    consultarPreco() 
+    print(f"O valor desse produto é: R${valorFixo}")
+
+  if processo == 4:
+    atualizar()
+
+  if processo == 5:
+      excluir() 
   
-if processo == 3:
-  consultarPreco() 
-  print(f"O valor desse produto é: R${preço}")
 
-if processo == 4:
-  atualizar()
+def letreiro():
+  print("---------------------------------------------------------------------------------------------------------------\n")
+  print("███╗░░░███╗███████╗██████╗░░█████╗░░█████╗░██████╗░░█████╗░  ██████╗░██╗░░░██╗████████╗██╗░░██╗░█████╗░███╗░░██\n████╗░████║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔══██╗╚██╗░██╔╝╚══██╔══╝██║░░██║██╔══██╗████╗░██\n██╔████╔██║█████╗░░██████╔╝██║░░╚═╝███████║██║░░██║██║░░██║  ██████╔╝░╚████╔╝░░░░██║░░░███████║██║░░██║██╔██╗██\n██║╚██╔╝██║██╔══╝░░██╔══██╗██║░░██╗██╔══██║██║░░██║██║░░██║  ██╔═══╝░░░╚██╔╝░░░░░██║░░░██╔══██║██║░░██║██║╚████\n██║░╚═╝░██║███████╗██║░░██║╚█████╔╝██║░░██║██████╔╝╚█████╔╝  ██║░░░░░░░░██║░░░░░░██║░░░██║░░██║╚█████╔╝██║░╚███║\n")
+  
+  print("---------------------------------------------------------------------------------------------------------------\n")
 
-if processo == 5:
-    excluir() 
+os.system("cls")
+letreiro()
+main()
